183437fbe80fc9f434dabf933ac8eafb1f4eb326
Andreas Soiron <Andy@Andys-MacBook-Pro.local>
2013-06-24 13:17:38 +0200

    init graph and show edges

--- src/bin/main.java
+++ src/bin/main.java
@@ -1,5 +1,11 @@
 package bin;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import lib.*;
+
 public class main {
 
 	/**
@@ -7,7 +13,33 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		
-
+		Node london = new Node("London");
+		Node berlin = new Node("Berlin");
+		Node wien = new Node("Wien");
+		Node rom = new Node("Rom");
+		Node paris = new Node(4, "Paris");
+		
+		List<Node> nodes = new ArrayList<Node>();
+		nodes.add(london);
+		nodes.add(berlin);
+		nodes.add(wien);
+		nodes.add(rom);
+		nodes.add(paris);
+		
+		List<Edge> edges = new ArrayList<Edge>();
+		edges.add(new Edge(london,berlin,100));
+		edges.add(new Edge(berlin,wien,200));
+		edges.add(new Edge(rom,paris,500));
+		
+		Graph graph = new Graph(nodes,edges);
+		
+		Iterator<Edge> edges_iterator = graph.getEdges().iterator();
+		
+		while(edges_iterator.hasNext()){
+			Edge edge = edges_iterator.next();
+			System.out.println("From "+edge.getA().getName()+" to "+edge.getB().getName()+ "distance "+edge.getDistance());
+		}
+		
 	}
 
 }
