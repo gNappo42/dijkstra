138434ec6692f48ba18ba35d99b26828483f5da0
Andreas Soiron <Andy@Andys-MacBook-Pro.local>
2013-06-26 10:12:45 +0200

    better view

--- src/view/Gui.java
+++ src/view/Gui.java
@@ -1,16 +1,11 @@
 package view;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.GridLayout;
-import java.util.List;
-
-import javax.swing.JButton;
+import java.util.Iterator;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import lib.Edge;
 import lib.Graph;
 import lib.Node;
 
@@ -34,24 +29,63 @@ public class Gui {
 	public void init(){
 		
 		this.main_frame = new JFrame();
-		JPanel content_panel = new JPanel();
-		JPanel navigation_panel = new JPanel();
-		//content_panel.setLayout(new GridLayout(5,4));
 		
-		//content_panel.add(new InputButton(")",this));
-		
-		NodesController nodes_controller = new NodesController(this);
-		EdgesController edges_controller = new EdgesController(this);
-		
-		//nodes_controller.index(this.graph.getNodes());
-		navigation_panel.add(BorderLayout.WEST,new NavigationButton("Locations",nodes_controller));
-		navigation_panel.add(BorderLayout.EAST,new NavigationButton("Routes",edges_controller));
-		
-		this.main_frame.getContentPane().add(BorderLayout.NORTH,content_panel);
-		this.main_frame.getContentPane().add(BorderLayout.SOUTH,navigation_panel);
+		JTabbedPane tabs = new JTabbedPane();
+		tabs.addTab("Locations", null, create_nodes_panel(), "Shows Locations");
+		tabs.addTab("Routes",null,create_routes_panel(), "Shows Edges");
+		this.main_frame.getContentPane().add(tabs);
 		this.main_frame.setSize(500,200);
 		this.main_frame.setVisible(true);
 
+	}
+	
+	public JPanel create_nodes_panel(){
+		Object[][] data = new Object[this.getGraph().getNodes().size()+1][3];
+		Iterator<Node> nodes_iterator = this.getGraph().getNodes().iterator();
+		
+		Object[] header_data ={"ID","Name"};
+		data[0] = header_data;
+		
+		int counter = 1;
+		while(nodes_iterator.hasNext()){
+			Node node = nodes_iterator.next();
+			Object[] node_data = {node.getId(),node.getName()};
+			data[counter] = node_data;
+			counter++;
+		}
+		
+		String[] columnNames = {"ID","Name"};
+		
+		JTable table = new JTable(data, columnNames);
+		
+		JPanel content_panel = new JPanel();
+		content_panel.add(table);
+		return content_panel;
+	}
+	
+	public JPanel create_routes_panel(){
+		Object[][] data = new Object[this.getGraph().getEdges().size()+1][4];
+		Iterator<Edge> edges_iterator = this.getGraph().getEdges().iterator();
+		
+		Object[] header_data ={"ID","A","B","Distance"};
+		data[0] = header_data;
+				
+		int counter = 1;
+		
+		while(edges_iterator.hasNext()){
+			Edge edge = edges_iterator.next();
+			Object[] node_data = {edge.getId(),edge.getA().getName(),edge.getB().getName(),edge.getDistance()};
+			data[counter] = node_data;
+			counter++;
+		}
+		
+		String[] columnNames = {"ID","A","B","Distance"};
+		
+		JTable table = new JTable(data, columnNames);
+		
+		JPanel content_panel = new JPanel();
+		content_panel.add(table);
+		return content_panel;
 	}
 
 }
