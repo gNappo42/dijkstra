97342ee965080ab980bcf2d5a8800c6d5500a9b8
Andreas Soiron <Andy@Andys-MacBook-Pro.local>
2013-07-03 09:56:58 +0200

    added position x and y

--- src/lib/Node.java
+++ src/lib/Node.java
@@ -6,6 +6,8 @@ public class Node {
 
 	private String name;
 	private int id;
+	private int position_x = 0;
+	private int position_y = 0;
 	
 	public Node(String name) {
 		this.id = idCounter++;
@@ -23,5 +25,21 @@ public class Node {
 	
 	public int getId() {
 		return this.id;
+	}
+
+	public int getPosition_y() {
+		return position_y;
+	}
+
+	public void setPosition_y(int position_y) {
+		this.position_y = position_y;
+	}
+
+	public int getPosition_x() {
+		return position_x;
+	}
+
+	public void setPosition_x(int position_x) {
+		this.position_x = position_x;
 	}
 }
